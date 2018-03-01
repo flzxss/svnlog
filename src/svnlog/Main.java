4e1ca02d81d2c8352af9476960b2ad8774c23d0b
iceroot <zxcyhn@126.com>
2018-03-01 23:42:45 +0800

    svnlog

--- src/svnlog/Main.java
+++ src/svnlog/Main.java
@@ -3,8 +3,8 @@ package svnlog;
 public class Main {
 
     public static void main(String[] args) {
-        String url = "svn://112.74.182.63/icessm";
-        String xls = "C:/Users/Administrator/Desktop/war005/aa/svninfo2.xls";
+        String url = "svn://127.0.0.1/web";
+        String xls = "C:/Users/Administrator/Desktop/svninfo.xls";
         SvnLog.log(url, xls);
     }
 
